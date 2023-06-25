1ac31145ae84734ac39598fa473dd086e2d16965
Aastha <aasthaagarwal2121@gmail.com>
2023-06-25 13:06:58 +0530

    connected frontend with the blockchain

--- src/App.js
+++ src/App.js
@@ -15,6 +15,19 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+    setAccount(accounts[0])
+    console.log(accounts[0])
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div>
 
