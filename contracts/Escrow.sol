844ab12985ac5d0be7d41fe26dc0ebbfaf4093df
Ishan <ishan.rakte@gmail.com>
2023-06-25 07:41:01 +0530

    contract added for listing properties

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,6 +15,16 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+   modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+
     constructor(
     address _nftAddress,
     address payable _seller,
@@ -24,5 +34,20 @@ contract Escrow {
         seller = _seller;
         inspector = _inspector;
         lender = _lender;
+    }
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller{
+        //NFT Transfer from seller to contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
     }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -29,6 +29,14 @@ describe('Escrow', () => {
             inspector.address,
             lender.address
         )
+
+        // Approve Property
+        transaction = await realEstate.connect(seller).approve(escrow.address, 1)
+        await transaction.wait()
+
+        // List Property
+        transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5))
+        await transaction.wait()
     })
 
     describe('Deployment', () => {
@@ -51,6 +59,33 @@ describe('Escrow', () => {
         it('Returns lender', async () => {
             const result = await escrow.lender()
             expect(result).to.be.equal(lender.address)
+        })
+    })
+    
+    describe('Listing', () => {
+
+        it('Updates as listed', async () => {
+            const result = await escrow.isListed(1)
+            expect(result).to.be.equal(true)
+        })
+
+        it('Updates Ownership', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+        })
+
+        it('Returns buyer', async () => {
+            const result = await escrow.buyer(1)
+            expect(result).to.be.equal(buyer.address)
+        })
+
+        it('Returns purchase price', async () => {
+            const result = await escrow.purchasePrice(1)
+            expect(result).to.be.equal(tokens(10))
+        })
+
+        it('Returns escrow amount', async () => {
+            const result = await escrow.escrowAmount(1)
+            expect(result).to.be.equal(tokens(5))
         })
     })
 })
