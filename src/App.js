6fb56da7826937c45d04471a51579f135a3968e4
Aastha <aasthaagarwal2121@gmail.com>
2023-06-25 13:31:14 +0530

    search bar and property component added

--- src/App.js
+++ src/App.js
@@ -35,10 +35,30 @@ function App() {
     <div>
 
       <Navigation account={account} setAccount={setAccount} />
-      
+      <Search />
       <div className='cards__section'>
 
-        <h3>Welcome to Millow</h3>
+        <h3>Home For You</h3>
+
+        <hr />
+
+<div className='cards'>
+    <div className='card'>
+      <div className='card__image'>
+        <img src= "" alt="Home" />
+      </div>
+      <div className='card__info'>
+        <h4> 1 ETH</h4>
+        <p>
+          <strong>1</strong> bedrooms |
+          <strong>2</strong> bathrooms |
+          <strong>3</strong> sqft
+        </p>
+        <p></p>
+      </div>
+    </div>
+
+</div>
 
       </div>
 
