cc08689c573c1139a81f375b38173d4306b29a4a
Aastha <aasthaagarwal2121@gmail.com>
2023-06-25 14:00:06 +0530

    contract added to finalize sale of property

--- test/Escrow.js
+++ test/Escrow.js
@@ -10,10 +10,10 @@ describe('Escrow', () => {
     let realEstate, escrow
 
     beforeEach( async () => {
-
+        
         //To Setup Accounts
         [buyer, seller, inspector, lender] = await ethers.getSigners()
-
+        
         //To Deploy the Real Estate
         const RealEstate = await ethers.getContractFactory('RealEstate')
         realEstate = await RealEstate.deploy()
@@ -122,11 +122,42 @@ describe('Escrow', () => {
             transaction = await escrow.connect(lender).approveSale(1)
             await transaction.wait()
         })
-
         it('Updates approval status', async () => {
             expect(await escrow.approval(1, buyer.address)).to.be.equal(true)
             expect(await escrow.approval(1, seller.address)).to.be.equal(true)
             expect(await escrow.approval(1, lender.address)).to.be.equal(true)
+        })
+    })
+    
+    describe('Sale', () => {
+        beforeEach(async () => {
+            let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
+            await transaction.wait()
+
+            transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
+            await transaction.wait()
+
+            transaction = await escrow.connect(buyer).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(seller).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(lender).approveSale(1)
+            await transaction.wait()
+
+            await lender.sendTransaction({ to: escrow.address, value: tokens(5) })
+
+            transaction = await escrow.connect(seller).finalizeSale(1)
+            await transaction.wait()
+        })
+
+        it('Updates ownership', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(buyer.address)
+        })
+
+        it('Updates balance', async () => {
+            expect(await escrow.getBalance()).to.be.equal(0)
         })
     })
 })
